ef218ae3240447e8ac6ed872d035b75f7d856c65
Emmanuel Tugado <emmanueltugado@gmail.com>
2024-11-03 23:54:44 +1100

    update library name

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-          name: "ConfidentialTechnology",
+          name: "ConfidentialTechnologyPackage",
           url: "https://etprogrammer-secret-intelligence-bucket-3.s3.ap-southeast-2.amazonaws.com/ConfidentialTechnology.xcframework.zip",
           checksum: "fb1e05bd8fa04f41d660feb8cbb41fdf59311b71730d54fd80a44b67e2973934"),
     ]
