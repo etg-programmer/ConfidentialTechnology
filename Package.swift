48dd04d0d61fdafa0d08fcd5c0e8494ff0f7ea7c
Emmanuel Tugado <emmanueltugado@gmail.com>
2024-11-03 23:45:43 +1100

    update library name

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ConfidentialTechnologyPackage-ETP",
-            targets: ["ConfidentialTechnologyPackage-ETP"]),
+            name: "ConfidentialTechnologyPackage",
+            targets: ["ConfidentialTechnologyPackage"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
