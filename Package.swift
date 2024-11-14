8af76ee3185623dd7d04c708d010d27a417dcf13
Emmanuel Tugado <emmanueltugado@gmail.com>
2024-11-15 00:38:58 +1100

    update package signature

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
           name: "ConfidentialTechnology",
-          url: "https://etgbucket2.s3.ap-southeast-2.amazonaws.com/ConfidentialTechnology.xcframework.zip",
+          url: "https://etgbucket3.s3.ap-southeast-2.amazonaws.com/ConfidentialTechnology.xcframework.zip",
           checksum: "5daf196e52b39bdadeee39a78affef18656b44e45bb517711ea2860d466b3883"),
     ]
 )
